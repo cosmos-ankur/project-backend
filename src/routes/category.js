f918dcba735e8edbe6d5bb3991920a2a9768c644
Ankur Singh <crazyankurcool@gmail.com>
2020-09-17 19:49:50 +0530

    initial setup

---
+++ src/routes/category.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const slugify = require('slugify');
+const { requireSignin, adminMiddileware } = require('../common-middleware');
+const { addCategory, getCategories } = require('../controller/category');
+const Category = require('../models/category');
+const shortid = require('shortid');
+const multer = require('multer');
+const path = require('path');
+
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.join(path.dirname(__dirname),'uploads'))
+    },
+    filename:function(req,file,cb){
+        cb(null,shortid.generate()+'-'+ file.originalname)
+    }
+});
+const upload = multer({storage});
+
+
+router.post('/category/create',requireSignin,adminMiddileware,upload.single('categoryImage'),addCategory)
+router.get('/category/getcategory',getCategories)
+module.exports = router;
