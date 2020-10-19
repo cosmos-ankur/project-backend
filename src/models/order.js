887133f89ac0a082a9505ea3113cf269d9562e24
Ankur Singh <crazyankurcool@gmail.com>
2020-10-19 23:36:06 +0530

    optimized

---
+++ src/models/order.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const orderSchema = new mongoose.Schema({
+    product: {type:mongoose.Schema.Types.ObjectId,ref:'Product',required:true},
+    quantity : {
+        type:Number,
+        required:true
+    }
+
+},{timestamps:true})
+module.exports = mongoose.model('Orders',orderSchema)
